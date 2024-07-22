08c74987ad21b8a65fbf871ce3a3512c45914eae
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-22 21:12:20 +0530

    logic changed to send the email properly

--- my_dashboard/src/controllers/applicationController.js
+++ my_dashboard/src/controllers/applicationController.js
@@ -8,30 +8,38 @@ const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
     user: 'podishettirakesh70@msitprogram.net',
-    pass: 'Rakesh*@*062',
   },
 });
 
 const submitApplication = async (req, res) => {
-  const { programId, email, personalDetails, educationalBackground, statementOfPurpose } = req.body;
+  const { program_id, email, personalDetails, educationalBackground, statementOfPurpose } = req.body;
   const status = 'pending';
+
+  // Ensure programId is correctly received
+  if (!program_id || isNaN(program_id)) {
+    console.log('Invalid program_id')
+    return res.status(400).json({ message: 'Invalid program_id' });
+
+  }
+
 
   try {
     const queryText = 'INSERT INTO applications (program_id, email, personal_details, educational_background, statement_of_purpose, status, submission_date) VALUES ($1, $2, $3, $4, $5, $6, NOW()) RETURNING *';
-    const newApplication = await pool.query(queryText, [programId, email, personalDetails, educationalBackground, statementOfPurpose, status]);
+    const newApplication = await pool.query(queryText, [program_id, email, personalDetails, educationalBackground, statementOfPurpose, status]);
 
     // Send confirmation email
     const mailOptions = {
-      from: 'your-email@gmail.com',
+      from: 'podishettirakesh70@msitprogram.net',
       to: email,
       subject: 'Application Submission Confirmation',
       text: `Dear Student,
 
-      Your application has been successfully submitted for program ID: ${programId}. It takes up to two days to verify the application. 
+      Your application has been successfully submitted for program ID: ${program_id}. It takes up to two days to verify the application. 
 
       Thank you for applying.
 
       Best regards,
+      Rakesh podishetti,
       Admissions Team`,
     };
 
