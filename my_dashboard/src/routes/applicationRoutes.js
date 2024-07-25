c2fa06efae554a0cbf92049229b4d9722351d54e
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-25 15:04:05 +0530

    modified new route to get student applications

--- my_dashboard/src/routes/applicationRoutes.js
+++ my_dashboard/src/routes/applicationRoutes.js
@@ -1,11 +1,11 @@
 // routes/applicationRoutes.js
 
 const express = require('express');
-const { submitApplication } = require('../controllers/applicationController');
+const { submitApplication, getStudentApplications } = require('../controllers/applicationController');
 const router = express.Router();
 
 router.post('/submit', submitApplication);
-router.get('/student', getStudentApplications); // New route for student applications
+router.get('/student', getStudentApplications); // New route for student applications/student', getStudentApplications); // New route for student applications
 
 
 module.exports = router;
